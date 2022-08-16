3b299b5558afda4e211ab601b3928748763068c2
thanh <thanhnd@vnext.com.vn>
2022-08-16 16:46:55 +0700

    update source code

--- src/main/java/com/example/springsecurityjwt/model/response/common/AbstractResponseFactory.java
+++ src/main/java/com/example/springsecurityjwt/model/response/common/AbstractResponseFactory.java
@@ -1,12 +1,14 @@
 package com.example.springsecurityjwt.model.response.common;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
 
 @Component
 public abstract class AbstractResponseFactory {
-    @Autowired
-    protected MessageSource ms;
+    protected final MessageSource ms;
+
+    public AbstractResponseFactory(MessageSource ms) {
+        this.ms = ms;
+    }
 }
 
